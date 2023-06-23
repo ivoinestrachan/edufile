d4e4bdbab40fdf59fad798ca7f64062087c8d92b
arihanv <arihanvaranasi@gmail.com>
2023-06-23 16:44:24 -0500

    Reorganization

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 // import { MoreVertical } from "lucide-react"
 
+import { Upload } from "lucide-react"
+import FileGrid from "@/components/file-grid"
+
 const data = [
   {
     title: "Example_PDF.pdf",
@@ -48,39 +51,13 @@ const data = [
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex flex-col items-center gap-2">
-        <div className="flex flex-wrap gap-5">
-          {data.map((item) => (
-            <div className="flex flex-col w-48 bg-slate-100 rounded-lg overflow-hidden border-4 hover:scale-105 delay-200 transition ease-in-out">
-              <div className="h-52 relative">
-                <img
-                  className="absolute h-full w-full"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN2CG1tEZrcR8Rx4LbX0gxX5QimvSe3aSJmUVubtd8rLNF4cowXm1fOcoY0UTOjgOU1kM&usqp=CAU"
-                  alt="Picture of the author"
-                />
-                <div className="absolute p-2 text-xs text-orange-700 font-bold">
-                  <div className="bg-orange-300 rounded-xl px-1.5 left-0">
-                    {item.course}
-                  </div>
-                </div>
-                {/* <button className="absolute p-2 text-xs right-0 text-black">
-              <MoreVertical/>
-            </button> */}
-              </div>
-              <div className="p-3">
-                <div className="font-semibold text-black">{item.title}</div>
-                <div className="flex gap-1 text-xs font-medium text-black items-center">
-                  {item.user}
-                  <div>
-                    <span className="text-gray-500 text-sm">·</span>
-                  </div>
-                  <span className="text-gray-500">{item.date}</span>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+      <div className="fixed right-0 bottom-0 left-0 p-10 flex justify-center">
+      <button className="bg-orange-300 text-orange-700 p-2 items-center text-xl rounded-xl font-bold border-2 flex gap-1.5 border-orange-700 hover:scale-125 transition ease-in-out">
+        <Upload/>
+       Upload File
+        </button>
       </div>
+      <FileGrid data={data}/>
     </section>
   )
 }
---
+++ components/file-grid.tsx
@@ -0,0 +1,51 @@
+import React from "react"
+
+type Props = {
+  data: File[]
+}
+
+interface File {
+  title: string
+  user: string
+  date: string
+  course: string
+  url: string
+}
+
+export default function FileGrid({ data }: Props) {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-wrap gap-5">
+        {data.map((item: File) => (
+          <div className="flex flex-col w-48 bg-slate-100 rounded-lg overflow-hidden border-4 hover:scale-105 transition ease-in-out">
+            <div className="h-52 relative">
+              <img
+                className="absolute h-full w-full"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN2CG1tEZrcR8Rx4LbX0gxX5QimvSe3aSJmUVubtd8rLNF4cowXm1fOcoY0UTOjgOU1kM&usqp=CAU"
+                alt="Picture of the author"
+              />
+              <div className="absolute p-2 text-xs text-orange-700 font-bold">
+                <div className="bg-orange-300 rounded-xl px-1.5 left-0">
+                  {item.course}
+                </div>
+              </div>
+              {/* <button className="absolute p-2 text-xs right-0 text-black">
+              <MoreVertical/>
+            </button> */}
+            </div>
+            <div className="p-3">
+              <div className="font-semibold text-black">{item.title}</div>
+              <div className="flex gap-1 text-xs font-medium text-black items-center">
+                {item.user}
+                <div>
+                  <span className="text-gray-500 text-sm">·</span>
+                </div>
+                <span className="text-gray-500">{item.date}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
