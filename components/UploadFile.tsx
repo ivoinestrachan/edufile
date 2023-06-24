f4ae21bf7635295e26371404567ef546feea3b8f
Ivoine <ivoinestrachan242@gmail.com>
2023-06-24 08:52:07 -0400

    fixed upload a little

--- components/UploadFile.tsx
+++ components/UploadFile.tsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 const UploadFile = () => {
+  const [file, setFile] = useState<File | undefined | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+  const [title, setTitle] = useState("");
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0]; // Use optional chaining to handle undefined case
+    setFile(selectedFile || null); // Set to null if selectedFile is undefined
+    setPreview(selectedFile ? URL.createObjectURL(selectedFile) : null);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Add your logic for handling the form submission here
+  };
+
   return (
     <div className="flex flex-col gap-5">
       <div>
-        <Input className="text-black" placeholder="Title of post..." />
+        <Input
+          className="text-black"
+          placeholder="Title of post..."
+          value={title}
+          onChange={handleTitleChange}
+        />
       </div>
       <div>
-        <Input type="file" />
+        <input type="file" onChange={handleFileChange} />
+        {preview && <Image src={preview} alt="Preview" className="mt-2" width={200} height={200} />}
       </div>
       <div>
-        <Button>Post</Button>
+        <Button onClick={handleSubmit}>Post</Button>
       </div>
     </div>
   );
