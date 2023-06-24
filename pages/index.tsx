91692fc703bd966ec9f884cd7a4c8ba272e099d4
Ivoine <ivoinestrachan242@gmail.com>
2023-06-24 07:57:56 -0400

    added alert button for to check if user is not signed in

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,16 @@ import {
 
 export default function Home({ feed }: HomeProps) {
   const { data: session } = useSession();
+
+  const handleUploadClick = () => {
+    if (session) {
+      // Show dialog for uploading files
+      // You can set a state or perform any other logic here
+    } else {
+      // Show toast notification to sign in
+      toast.error("Please sign in to upload");
+    }
+  };
   
   return (
     <>
@@ -28,21 +38,32 @@ export default function Home({ feed }: HomeProps) {
       <section className="items-center gap-6 pb-8 pt-6 md:py-10 flex flex-col justify-center p-10">
         <SearchBar />
         <div className="fixed z-50 right-0 bottom-0 left-0 p-10 flex justify-center">
-          <Dialog>
-            <DialogTrigger disabled={session ? false : true} asChild>
-              <button
-                className="bg-orange-300 text-orange-700 p-2 items-center text-xl rounded-xl font-bold border-2 flex gap-1.5 border-orange-700 hover:scale-125 transition ease-in-out cursor-pointer"
-              >
-                <Upload />
-                Upload File
-              </button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <div className="text-black">
-                <UploadFile />
-              </div>
-            </DialogContent>
-          </Dialog>
+          {session ? (
+            <Dialog>
+              <DialogTrigger asChild>
+                <button
+                  className="bg-orange-300 text-orange-700 p-2 items-center text-xl rounded-xl font-bold border-2 flex gap-1.5 border-orange-700 hover:scale-125 transition ease-in-out cursor-pointer"
+                  onClick={handleUploadClick}
+                >
+                  <Upload />
+                  Upload File
+                </button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <div className="text-black">
+                  <UploadFile />
+                </div>
+              </DialogContent>
+            </Dialog>
+          ) : (
+            <button
+              className="bg-orange-300 text-orange-700 p-2 items-center text-xl rounded-xl font-bold border-2 flex gap-1.5 border-orange-700 hover:scale-125 transition ease-in-out cursor-pointer"
+              onClick={() => toast.error("Please sign in to upload")}
+            >
+              <Upload />
+              Upload File
+            </button>
+          )}
         </div>
         {/* // @ts-ignore */}
         <FileGrid data={feed} />
