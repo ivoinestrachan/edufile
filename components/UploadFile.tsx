3c918b35eec9bc59f2d3563965daab03ffbd46dc
Ivoine <ivoinestrachan242@gmail.com>
2023-06-23 22:57:26 -0400

    fixed build error

--- components/UploadFile.tsx
+++ components/UploadFile.tsx
@@ -1,18 +1,22 @@
-const UploadFile = ({ onClose }) => {
-    return (
-        <div>
-           <div>
-            <input  placeholder="title"/>
-           </div>
-           <div>
-            <input type="file" />
-           </div>
-           <div>
-            <button>Post</button>
-           </div>
-           <button onClick={onClose} className="text-[24px] font-bold">X</button>
-        </div>
-    );
-}
+import React from 'react';
+
+const UploadFile = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <div>
+      <div>
+        <input placeholder="title" />
+      </div>
+      <div>
+        <input type="file" />
+      </div>
+      <div>
+        <button>Post</button>
+      </div>
+      <button onClick={onClose} className="text-[24px] font-bold">
+        X
+      </button>
+    </div>
+  );
+};
 
 export default UploadFile;
