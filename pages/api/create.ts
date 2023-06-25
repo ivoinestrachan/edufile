8d6b817a20739277a7a9de905335341b320c0c51
Ivoine <ivoinestrachan242@gmail.com>
2023-06-25 09:21:33 -0400

    updated auth

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,6 +1,6 @@
 import prisma from "../../db/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/next";
+import { getSession } from "next-auth/react";
 
 export default async function handler(
   req: NextApiRequest,
